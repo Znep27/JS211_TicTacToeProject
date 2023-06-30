0899ea1000c5f95d2907af8f0df0883efdd24bb9
Marc Rozenburgh <znep27@hotmail.com>
2023-06-29 19:51:43 -0500

    update dom script

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -38,6 +38,7 @@ const addMarker = (id) => {
   
   // Arrange the above pieces into a single line of code
   // to add an X or O to the board to the DOM so it can be scene on the screen.
+  document.getElementById(id).innerHTML = currentMarker;
 }
 
 // passes the element's id attribute from HTML to be used
@@ -51,6 +52,7 @@ const updateBoard = (id) => {
 
   // @TODO, Your code here: use the above information to change the board variable(array of arrays)
   // HINT: in your browser open up the dev tools -> console
+  board[row][column] = currentMarker;
 }
 
 const checkForWin = () => {
@@ -66,14 +68,45 @@ const checkForWin = () => {
 
 const horizontalWin = () => {
   // @TODO, Your code here: to check for horizontal wins
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+    || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+    || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+    || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+    || (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+    || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+    ) { 
+    return true; 
+  } else {
+    return false;
+  }
 }
 
 const verticalWin = () => {
   // @TODO, Your code here: to check for vertical wins
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X")
+    || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+    || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
+    || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+    || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X")
+    || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+    ) { 
+      return true; 
+  } else {
+      return false;
+  }
 }
 
 const diagonalWin = () => {
   // @TODO, Your code here: to check for diagonal wins
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  || (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+  ) { 
+    return true; 
+  } else {
+    return false;
+  }
 }
 
 const changeMarker = () => {
@@ -95,6 +128,12 @@ const resetBoard = () => {
   }
   
   // @TODO, Your code here: make sure to reset the array of arrays to empty for a new game
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ]
+  currentMarker = 'X'
 }
 
 // **BONUSES**
